3ab554974e3e9a4963ec3d79c4650e56ed57464a
Jordan Ulmer <julmer526@gmail.com>
2022-03-06 15:51:21 +0000

    First functions created

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Test")
+function add(a,b){
+    return a + b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+function operate(a, operator, b){
+    if (operator == "+") {
+        return add(a,b)
+    }else if (operator == "-"){
+        return subtract(a,b)
+    }else if (operator == "*") {
+        return multiply(a,b)
+    }else if (operator == "/") {
+        return divide(a,b)
+    }else {
+        return alert("That didn't work")
+}}
